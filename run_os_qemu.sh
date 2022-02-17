b4561b35986e8dbd7ad385909032f7ca26ca31ad
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2022-02-17 19:55:34 +0000

    Updated qemu script to support debug flag

--- run_os_qemu.sh
+++ run_os_qemu.sh
@@ -1,1 +1,13 @@
-qemu-system-x86_64 -cdrom build/os.iso -monitor stdio -s
+# runs qemu with a few parameters
+# -d = run qemu with support for gdb debugging (opens a port on localhost:1234)
+
+extraflags="-monitor stdio"
+
+while getopts d: flag
+do
+    case "${flag}" in
+        d) extraflags="${extraflags} -s";;
+    esac
+done
+
+qemu-system-x86_64 -cdrom build/os.iso ${extraflags}
