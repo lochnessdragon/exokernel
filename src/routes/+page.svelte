f8c867caf4390691d8a6fbdd733ab41df6d1fa1e
lochnessdragon <39635811+lochnessdragon@users.noreply.github.com>
2023-04-13 11:20:22 -0500

    Added custom colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,13 +11,15 @@
 
 <!-- Download/header -->
 <section id="banner">
-	<div class="text-left min-h-screen">
-		<h1 class="text-5xl md:text-8xl mb-4 font-bold">
-			Exokernel
-		</h1>
-		<div class="justify-center">
-			<button class="inline-svg rounded-lg py-2 px-6 text-center bg-gradient-to-br from-indigo-700 to-purple-900 text-white">Download .iso {@html feather.icons.download.toSvg({'width': 18, 'height': 18})}</button>
-			<p class="text-slate-700">Latest Version: <span class="underline">v0.0.1</span></p>
+	<div class="min-h-screen flex items-center">
+		<div class="flex-grow px-8 text-center md:text-left">
+			<h1 class="text-5xl md:text-8xl mb-4 font-bold">
+				Exokernel
+			</h1>
+			<div class="justify-center">
+				<button class="inline-svg rounded-lg py-2 px-6 text-center bg-gradient-to-br from-indigo-700 to-purple-900 text-white">Download .iso {@html feather.icons.download.toSvg({'width': 18, 'height': 18})}</button>
+				<p class="text-slate-700">Latest Version: <span class="underline">v0.0.1</span></p>
+			</div>
 		</div>
 	</div>
 </section>
@@ -33,19 +35,19 @@
 </section>
 
 <!-- Guides and Documentation -->
-<section id="docs" class="bg-gray-900 text-white pt-4 p-1">
-	<h2 class="font-bold text-4xl md:text-6xl text-amber-300">Guides:</h2>
+<section id="docs" class="bg-manilla-paper text-inky-black pt-4 p-1">
+	<h2 class="font-bold text-4xl md:text-6xl text-mars-red">Guides:</h2>
 	<!-- Getting started -->
 	<!-- How to write a lib os -->
 	<!-- Compiling the project -->
 	<p>
-		In order to start your journey into the stars, I suggest <a class="underline text-amber-300"><i>The Beginner's Guide by R. U. Reddy</i></a>. If you are a developer looking to compile the project, there's is no better guide than <a class="underline text-amber-300"><i>Birthing your own Exokernel by Bertha Pangs</i></a>. Finally, if you are looking to grow your own library operating system, check out <a class="underline text-amber-300"><i>Cultivating a healthy Library (OS) by Chist M. Dev</i></a>.
+		In order to start your journey into the stars, I suggest <a class="underline text-mars-red"><i>The Beginner's Guide by R. U. Reddy</i></a>. If you are a developer looking to compile the project, there's is no better guide than <a class="underline text-mars-red"><i>Birthing your own Exokernel by Bertha Pangs</i></a>. Finally, if you are looking to grow your own library operating system, check out <a class="underline text-mars-red"><i>Cultivating a healthy Library (OS) by Chist M. Dev</i></a>.
 	</p>
 	
-	<h2 class="font-bold text-4xl md:text-6xl mt-4 text-indigo-300">Documentation:</h2>
+	<h2 class="font-bold text-4xl md:text-6xl mt-4 text-canyon-orange">Documentation:</h2>
 	<!-- API Documentation -->
 	<p>
-		Fuddled over functions? Mixed up by methods? Lacking a general sense of direction through the forest of knowledge? Maybe the <a class="underline text-indigo-300">Definitely Definitive Dive into the API</a> will help.
+		Fuddled over functions? Mixed up by methods? Lacking a general sense of direction through the forest of knowledge? Maybe the <a class="underline text-canyon-orange">Definitely Definitive Dive into the API</a> will help.
 	</p>
 </section>
 
--- windi.config.ts
+++ windi.config.ts
@@ -6,6 +6,17 @@ export default defineConfig({
 			header: ['Poiret One'],
 			body: [],
 			code: []
+		},
+		extend: {
+			colors: {
+				'manilla-paper': '#F9EFD9',
+				cardstock: '#BFA182',
+				'sun-yellow': '#FFF38D',
+				'canyon-orange': '#FF8D28',
+				'mars-red': '#FF2D0D',
+				'inky-black': '#2D2D2D',
+				'marble-white': '#FEFAFE'
+			}
 		}
 	}
 });
